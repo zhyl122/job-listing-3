265542cc73346f5a7a26550793a8421c84c6bf4f
zhouyilin <zhyl9697@gmail.com>
2017-01-21 12:30:57 +0800

    重构admin 条件 放到model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+def admin?
+  email == 'zhyl9697@163.coom'
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  namespace :admin do
+
   resources :jobs
   root 'welcome#index'
 end
