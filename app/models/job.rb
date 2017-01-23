1f29a747c27e75a3b16e15f454293b875c724277
zhouyilin <zhyl9697@gmail.com>
2017-01-23 19:24:42 +0800

    implement step5-1

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,9 +1,4 @@
 class Job < ApplicationRecord
-  validates :title, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
-  
   def publish!
     self.is_hidden = false
     self.save
